9e2166def8022db3ac0e1edb86e5d7e950adae1d
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-20 15:30:06 -0300

    Início do método `iniciarLocalizacao` e permissões adicionadas

--- app/src/main/java/com/oceanbrasil/ocean_android_gps_20_01_2021/MapsActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_gps_20_01_2021/MapsActivity.kt
@@ -1,8 +1,12 @@
 package com.oceanbrasil.ocean_android_gps_20_01_2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -40,5 +44,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(samsungOceanSp).title("Samsung Ocean SP - Poli"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 18f))
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(samsungOceanSp, 18f))
+
+        iniciarLocalizacao()
+    }
+
+    private fun iniciarLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationProvider = LocationManager.GPS_PROVIDER
+
+        val ultimaLocalizacao = locationManager.getLastKnownLocation(locationProvider)
     }
 }
